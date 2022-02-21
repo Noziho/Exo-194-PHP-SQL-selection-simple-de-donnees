41b472dc4f9c6fc26858bb391b55cd36fc8480cd
Noah2211 <noah.decroix3@gmail.com>
2022-02-21 16:45:26 +0100

    First step done

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-
+require __DIR__ . '/Config.php';
+require __DIR__ . '/DB_Connect.php';
 /**
  * 1. Importez le fichier SQL se trouvant dans le dossier SQL.
  * 2. Connectez vous à votre base de données avec PHP
@@ -13,3 +14,25 @@
  * 4. Faites la même chose, mais cette fois ci, triez le résultat selon la colonne ID, du plus grand au plus petit.
  * 5. Faites la même chose, mais cette fois ci en ne sélectionnant que les noms et les prénoms.
  */
+
+
+$stmt = DB_Connect::dbConnect()->prepare("
+    SELECT * FROM user
+");
+
+
+
+if ($stmt->execute()) {
+    foreach ($stmt->fetchAll() as $user) { ?>
+        <div>Utilisateur: <?= $user['nom'] . " " . $user['prenom'] . "<br>" .
+        "Rue: " . $user['rue'] . "<br>" .
+        "Numero: " . $user['numero']. "<br>".
+        "Code postal: " . $user['code_postal'] . "<br>" .
+        "Ville: " . $user['ville'] . "<br>" .
+        "Pays: " . $user['pays'] . "<br>" .
+        "Mail: " .$user['mail'] . "<br>" . "<br><br>"
+
+        ?></div><?php
+
+    }
+}
